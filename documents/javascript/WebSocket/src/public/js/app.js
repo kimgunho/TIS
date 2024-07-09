8fec9234e5d222c9fb79b03278d3ff493f8e0d12
kim <yuyukgh@naver.com>
2024-07-10 03:23:19 +0900

    add sample client socket with server socket

--- documents/javascript/WebSocket/src/public/js/app.js
+++ documents/javascript/WebSocket/src/public/js/app.js
@@ -1,1 +1,13 @@
-alert("hi");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+  console.log("Connected To Server open");
+});
+
+socket.addEventListener("message", (msg) => {
+  console.log("got this", msg.data);
+});
+
+socket.addEventListener("close", () => {
+  console.log("disconnected To Server X");
+});
--- documents/javascript/WebSocket/src/server.js
+++ documents/javascript/WebSocket/src/server.js
@@ -6,11 +6,17 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log("listen on http://localhose:3000");
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (socket) => {
+  socket.on("close", () => console.log("disconnected from browser"));
+  socket.send("hello");
+});
+
 server.listen(3000, handleListen);
