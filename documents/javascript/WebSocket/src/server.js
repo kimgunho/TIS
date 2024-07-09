f2cf497d3cfcaad869949cd5b68293ad6e45c497
kim <yuyukgh@naver.com>
2024-07-10 02:53:16 +0900

    connect ws

--- documents/javascript/WebSocket/src/server.js
+++ documents/javascript/WebSocket/src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -9,4 +11,6 @@ app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log("listen on http://localhose:3000");
 
-app.listen(3000, handleListen);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+server.listen(3000, handleListen);
